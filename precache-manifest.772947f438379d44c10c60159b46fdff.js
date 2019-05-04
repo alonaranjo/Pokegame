6003ebcdb9b9a36c0058eaf01e7d5ce69afa1316
alonaranjo <alonso301090@gmail.com>
2019-05-04 10:30:02 -0600

    Updates

---
+++ precache-manifest.772947f438379d44c10c60159b46fdff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c3d971f2cc50f4ba18d2d41ef343dd",
+    "url": "/Pokegame/index.html"
+  },
+  {
+    "revision": "ace4c7b77ea4815af48d",
+    "url": "/Pokegame/static/css/main.94c8ef4f.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/Pokegame/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "ace4c7b77ea4815af48d",
+    "url": "/Pokegame/static/js/main.7e5eb384.chunk.js"
+  },
+  {
+    "revision": "3edffc7b8e72a9691d06",
+    "url": "/Pokegame/static/js/runtime~main.7c53fbd9.js"
+  }
+]);
